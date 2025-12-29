f447621f6627bd0b0eab5e947a3862e66cb302c4
Martin Graf <fairrelex@gmail.com>
2025-12-29 18:32:39 -0300

    agregamos un rango de edades utilizando constantes

--- 6.RegistroDeNotas/PedidoDeDatosAlUsuario.py
+++ 6.RegistroDeNotas/PedidoDeDatosAlUsuario.py
@@ -1,5 +1,9 @@
 #Hacemos una funcion que pide al usuario su edad como para practicar, lo devuelve a la funcion y ya se puede utilizar
 
+#Creamos constantes para los minimos y maximos de la edad
+EDAD_MINIMA = 1
+EDAD_MAXIMA = 120
+
 def PedirEdadUsuario():
     #creamos el bucle para que siga pidiendo la edad hasta que se ingrese un valor valido
     while True:
@@ -10,6 +14,8 @@ def PedirEdadUsuario():
             #chequeamos que la edad no sea negativa
             if edad < 0:
                 print("La edad no puede ser negativa. Intente nuevamente.")
+            elif edad < EDAD_MINIMA or edad > EDAD_MAXIMA:
+                print(f"La edad debe estar entre {EDAD_MINIMA} y {EDAD_MAXIMA}. Intente nuevamente.")
             else:
                 return edad
         #manejamos el error de conversion
