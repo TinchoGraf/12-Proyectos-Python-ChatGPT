1883393584c3654e80c4645238fa140eeff65247
Martin Graf <fairrelex@gmail.com>
2025-12-29 18:29:41 -0300

    le agregamos comentarios pertinentes para que se entienda mejor el codigo

--- 6.RegistroDeNotas/PedidoDeDatosAlUsuario.py
+++ 6.RegistroDeNotas/PedidoDeDatosAlUsuario.py
@@ -1,11 +1,18 @@
+#Hacemos una funcion que pide al usuario su edad como para practicar, lo devuelve a la funcion y ya se puede utilizar
+
 def PedirEdadUsuario():
+    #creamos el bucle para que siga pidiendo la edad hasta que se ingrese un valor valido
     while True:
+        #encerramos todo en un bloque try-except para manejar errores de conversion
         try:
+            #convertimos la entrada a entero
             edad = int((input("Por favor, ingrese su edad: ")))
+            #chequeamos que la edad no sea negativa
             if edad < 0:
                 print("La edad no puede ser negativa. Intente nuevamente.")
             else:
                 return edad
+        #manejamos el error de conversion
         except ValueError:
             print("Entrada inválida. Por favor, ingrese un número entero para la edad.")
 
