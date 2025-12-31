dd606bd7a7f417b5e329419db5d8b5c1c01c3ea0
Martin Graf <fairrelex@gmail.com>
2025-12-30 21:04:27 -0300

    se agrego todo lo referente a manejo de errores, ingresos erroneos y control desde el main, devolviendo diccionarios en cada return y haciendo que el main interprete estos

--- 6.RegistroDeNotas/Validaciones.py
+++ 6.RegistroDeNotas/Validaciones.py
@@ -9,35 +9,32 @@ CORTE = "*"
 
 #Funcion generica que pide un dato y lo valida con una funcion de validacion pasada por parametro
 def PedirDato(mensaje, funcionValidacion):
-    #ciclamos hasta que el dato sea valido
-    while True:
+    
             #pedimos el dato al usuario recordandole la opcion de corte
-            print(f"Ingrese '{CORTE}' para finalizar el ingreso.")
             dato = input(mensaje)
 
-            #si el usuario ingresa el dato de corte, devolvemos None
             if dato == CORTE:
-                break
+                return {"valido": False, "estado": "corte"}
+
             #enviamos el dato a validar a la funcion de validacion
             datoValido = funcionValidacion(dato)
 
-            #si el dato es valido, lo devolvemos
-            if datoValido["valido"]:
-                return datoValido["valor"]
+            return datoValido
             
-            #si no es valido, mostramos el mensaje de error
-            else:
-                print(datoValido["mensajeError"])
 
 #Funcion especifica que valida el nombre
 def ValidarNombre(nombre):
+
+    
     #limpiamos espacios en blanco al inicio y al final
     nombreLimpio = nombre.strip()
+
 
     #chequeamos que el nombre no este vacio
     if nombreLimpio == "":
         #devolvemos un diccionario con el estado de la validacion
         return {"valido": False,
+                "estado": "error",
                  "mensajeError": "El nombre no puede ser solo espacios en blanco. Intente nuevamente."
         }    
     
@@ -45,12 +42,15 @@ def ValidarNombre(nombre):
     elif any(char.isdigit() for char in nombreLimpio):
         #devolvemos un diccionario con el estado de la validacion
         return {"valido": False,
+                "estado": "error",
                  "mensajeError": "El nombre no puede contener números. Intente nuevamente."
         }
     
 
     #si todo esta bien, devolvemos el nombre limpio en un diccionario
-    return {"valido": True, "valor": nombreLimpio}
+    return {"valido": True,
+            "estado": "ok",
+             "valor": nombreLimpio}
 
 #Funcion especifica que valida la edad
 def ValidarEdad(edad):
@@ -58,22 +58,30 @@ def ValidarEdad(edad):
 
     #Hacemos el try-except para manejar errores de conversion
     try:    
+       
         #convertimos la entrada a entero
         edadInt = int(edad)
+
         #chequeamos que la edad no sea negativa
         if edadInt < 0:
-            return {"valido": False, "mensajeError": "La edad no puede ser negativa. Intente nuevamente."}
+            return {"valido": False,
+                    "estado": "error", 
+                    "mensajeError": "La edad no puede ser negativa. Intente nuevamente."}
         #chequeamos que la edad este dentro de los limites
         elif edadInt < EDAD_MINIMA or edadInt > EDAD_MAXIMA:
             return {"valido": False,
+                    "estado": "error",
                      "mensajeError": f"La edad debe estar entre {EDAD_MINIMA} y {EDAD_MAXIMA}. Intente nuevamente."
             }
         #si todo esta bien, devolvemos la edad en un diccionario
-        return {"valido": True, "valor": edadInt}
+        return {"valido": True,
+                "estado": "ok",
+                "valor": edadInt}
 
     #manejamos el error de conversion    
     except ValueError:
         return {"valido": False,
+                "estado": "error",
                  "mensajeError": "Entrada inválida. Por favor, ingrese un número entero para la edad."
         }
     
@@ -83,15 +91,30 @@ def main():
     #agregamos una lista vacia donde guardaremos los datos
     edades = []
 
+    print("Ingrese las edades de las personas que quiera guardar.")
+    print(f"Para finalizar el ingreso, ingrese '{CORTE}'.")
+
     #metemos todo en un bucle para permitir multiples ingresos
     while True:
+        """
         #pedimos el nombre al usuario usando la funcion generica
         nombre = PedirDato("Por favor, ingrese su nombre: ", ValidarNombre)
+        """
         #pedimos la edad al usuario usando la funcion generica
         edad = PedirDato("Por favor, ingrese las edades que quiera guardar: ", ValidarEdad)
 
-        #agregamos las edades validas a la lista
-        edades.append(edad)
+        if edad["estado"] == "corte":
+            print("Ingreso finalizado.")
+            break
+
+        elif edad["estado"] == "error":
+            print(edad["mensajeError"])
+            continue
+        
+        elif edad["estado"] == "ok":
+            #agregamos las edades validas a la lista
+            edades.append(edad["valor"])
+
 
     """"
     #mostramos los datos ingresados
