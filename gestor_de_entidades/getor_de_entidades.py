ac095c73d6a6f9613bbb0d95af12cb031029d835
notebook Tincho <fairrelex@gmail.com>
2026-02-18 20:46:42 -0300

    se agregaron las funciones de guardado y cargado en archivo, persistencia

--- gestor_de_entidades/getor_de_entidades.py
+++ gestor_de_entidades/getor_de_entidades.py
@@ -1,4 +1,7 @@
+#Hacemos los imports necesarios
 
+#Para guardar en formato json, vamos a usar la libreria json que viene incluida en python, no es necesario instalar nada
+import json 
 
 #Creamos una lista vacia para pasar por parametro, luego se eliminará para cargarla y descargarla desde un archivo
 lista_personas = []
@@ -132,3 +135,22 @@ def controlador(accion, dato, lista_personas):
     else:
         return "Accion no valida"
     
+
+#Creamos una funcion para guardar la lista en un archivo
+def guardar_en_archivo(lista_personas, nombre_archivo):
+    try:
+        with open(nombre_archivo, "w") as archivo:
+            json.dump(lista_personas, archivo, indent=4)
+            return { "estado": "ok", "accion": "guardar", "mensaje": "Lista guardada exitosamente", "data": lista_personas }
+    except Exception as e:
+        return { "estado": "error", "accion": "guardar", "mensaje": f"No se pudo guardar la lista: {str(e)}", "data": None }
+    
+    
+#Creamos una funcion para cargar la lista desde un archivo
+def cargar_desde_archivo(nombre_archivo):
+    try:
+            with open(nombre_archivo, "r") as archivo:
+                lista_personas = json.load(archivo)
+            return { "estado": "ok", "accion": "cargar_desde_archivo", "mensaje": "Lista cargada exitosamente", "data": lista_personas }
+    except FileNotFoundError:
+        return { "estado": "error", "accion": "cargar_desde_archivo", "mensaje": "No se encontro el archivo", "data": None }
